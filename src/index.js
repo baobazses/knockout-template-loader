5a00cca9004dc89227a2bc69878a615968fed0ba
antoine-duc <a.duc92@gmail.com>
2017-04-27 10:59:18 +0200

    set module.exports received as parameter with html string

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import utils from "loader-utils";
 function loaderFn(source) {
 	this.cacheable();
 
-	const sourcePart = source.replace("module.exports", "var htmlContent");
 	const options = utils.getLoaderConfig(this, "knockoutTemplateLoader");
 	const name = options.name || utils.interpolateName(this, "[name]-[ext]", {});
 
 	return [
 		"var ko = require('knockout');",
 		"var stringTemplateEngine = require('knockout-template-loader/lib/string-template-engine');",
-		sourcePart,
-		`ko.templates['${name}'] = htmlContent;`
+		source,
+		`ko.templates['${name}'] = module.exports;`
 	].join("\n");
 }
 
