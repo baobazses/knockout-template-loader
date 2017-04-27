bfb90d9b4c4f563bcb1bc611f012aead86db47cd
antoine-duc <a.duc92@gmail.com>
2017-04-27 11:25:06 +0200

    removed es6 code (import syntax issue in webpack)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import utils from "loader-utils";
+var utils = require("loader-utils");
 
 function loaderFn(source) {
 	this.cacheable();
 
-	const options = utils.getLoaderConfig(this, "knockoutTemplateLoader");
-	const name = options.name || utils.interpolateName(this, "[name]-[ext]", {});
+	var options = utils.getLoaderConfig(this, "knockoutTemplateLoader");
+	var name = options.name || utils.interpolateName(this, "[name]-[ext]", {});
 
 	return [
 		"var ko = require('knockout');",
-		"var stringTemplateEngine = require('knockout-template-loader/lib/string-template-engine');",
+		"var stringTemplateEngine = require('knockout-template-loader/src/string-template-engine');",
 		source,
 		`ko.templates['${name}'] = module.exports;`
 	].join("\n");
